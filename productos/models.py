febdd7e178a590a86a64ba665e53ef4baf8b0598
fyllstoff <pabl.bravoa@gmail.com>
2024-10-21 08:54:11 -0300

    Actualizar models.py

--- productos/models.py
+++ productos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     marca = models.CharField(max_length=255)
-    precio = models.TextField()  # O CharField si prefieres
+    precio = models.TextField()  
 
     def __str__(self):
         return self.nombre
